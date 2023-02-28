6d79eb9edb4d5e0b0ac990a99c542fca38de11b0
Giovanni Andrade Nespoli <ggnespoli@gmail.com>
2023-02-27 21:16:05 -0300

    feat: validations

--- projeto_sil/src/scripts/totalValueScript.js
+++ projeto_sil/src/scripts/totalValueScript.js
@@ -1,13 +1,30 @@
 var values = document.querySelectorAll(".values")
 
 values.forEach(vls => {
+
     var value = vls.querySelector(".value").textContent
     vls.querySelector(".value").textContent = parseInt(value).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
 
     var quantity = vls.querySelector(".quantity").textContent
 
+    if (isNaN(value)) {
+        var invaidNumber = value.replace('R$', '')
+
+        vls.querySelector(".value").textContent = parseInt(invaidNumber).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+
+        var sum = invaidNumber * quantity;
+        return vls.querySelector(".totalValue").textContent = sum.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+    }
+
+    if (isNaN(quantity)) {
+        var user = vls.querySelector(".user").textContent
+        var product = vls.querySelector(".product").textContent
+
+        console.log("Insira um valor certo. Erro no pedido do usúario: " + user + " pedindo um(a) " + product)
+        vls.querySelector(".quantity").textContent = "Valor inválido"
+        return vls.querySelector(".totalValue").textContent = parseInt(value).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+    }
+
     var sum = value * quantity
     vls.querySelector(".totalValue").textContent = sum.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
-
-    console.log("QUANTIDADE: " + quantity + "; VALUE: " + value + "; TOTALVALUE: " + sum);
 });
